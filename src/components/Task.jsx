5c4c3d85c3d2d4097722823379eeaec05390be3d
andrew hulme <andrew_hulme04@hotmail.com>
2025-03-03 10:31:24 +1100

    Added Task.jsx

---
+++ src/components/Task.jsx
@@ -0,0 +1,14 @@
+import { useDraggable } from "@dnd-kit/core";
+
+export default function Task({ task, index }) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({ id: task.id });
+
+  return (
+    <div ref={setNodeRef} {...listeners} {...attributes}
+    style={{ transform: transform ? `translate(${transform.x}px, ${transform.y}px)` : "none"}}
+    className="bg-white p-2 rounded shadow mb-2 cursor-grab"
+    >
+      {task.content}
+    </div>
+  );
+}
