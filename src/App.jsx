3829148e3b612f5e7e7971dde81d24473313e10c
andrew hulme <andrew_hulme04@hotmail.com>
2025-03-03 17:18:58 +1100

    Fixed syntax error in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import KanbanBoard from "./components/KanbanBoard";
 import './App.css'
 
-export default funtion App() {
+export default function App() {
   return (
     <div className='p-4'>
       <h1 className='text-2xl font-bold mb-4'>Project Planner</h1>
