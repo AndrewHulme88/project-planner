1ef3e3e217225fb2fdde981ce79d00ae97e9a288
Andrew Hulme <andrew_hulme04@hotmail.com>
2025-03-26 21:34:09 +1100

    Modified LandingPage.jsx to hide tasks button if not logged in

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
 
-const LandingPage = () => (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h2>Welcome to Project Manager</h2>
-        <p>Track your tasks. Stay productive.</p>
-        <Link to="/login">
-            <button style={{ marginRight: "10px" }}>Login / Sign Up</button>
-        </Link>
-        <Link to="/tasks">
-            <button>Go to Tasks</button>
-        </Link>
-    </div>
-);
+const LandingPage = () => {
+    const token = localStorage.getItem("token");
+
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h2>Welcome to Project Manager</h2>
+            <p>Track your tasks. Stay productive.</p>
+
+            {!token ? (
+                <Link to="/login">
+                    <button style={{ marginRight: "10px" }}>Login / Sign Up</button>
+                </Link>
+            ) : (
+                <Link to="/tasks">
+                    <button>Go to Tasks</button>
+                </Link>
+            )}
+        </div>
+    );
+};
+    
 
 export default LandingPage;
